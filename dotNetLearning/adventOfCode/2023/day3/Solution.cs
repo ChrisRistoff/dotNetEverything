6ca8fd119585719319b5f0a716aed170316d9433
Krasyo <doanusho@gmail.com>
2023-12-05 12:23:10 +0000

    still not getting the right result but at least it's working again...

--- dotNetLearning/adventOfCode/2023/day3/Solution.cs
+++ dotNetLearning/adventOfCode/2023/day3/Solution.cs
@@ -1,10 +1,11 @@
 public class AdventDayThree
 {
+
     public string[] GetData()
     {
         string filepath = "./adventOfCode/2023/day3/testData.txt";
-        string[] result = { "empty" };
-        string[] lastRemoved = { "temp" };
+        string[] result = {};
+        string[] lastRemoved = {};
 
         try
         {
@@ -25,7 +26,6 @@ public class AdventDayThree
 
         return lastRemoved;
     }
-
     public void GetSum(string[] data)
     {
         int sum = 0;
@@ -35,42 +35,45 @@ public class AdventDayThree
             string currNum = "";
             for (int j = 0; j < currLine.Length; j++)
             {
-                currNum = "";
-                int before = j;
-                while (char.IsDigit(currLine[j]))
+                if (char.IsDigit(currLine[j]))
                 {
                     currNum += currLine[j];
-                    j++;
                 }
-
-                int after = j;
-
-                if (currLine[j] != '.' && !char.IsDigit(currLine[j]))
+                else
                 {
-                    sum += int.Parse(currNum);
-                    currNum = "";
-                }
-                else if (!char.IsDigit(currLine[j]) && currNum != "" && currLine[j] != '.')
-                {
-                    int prevLine = i - 1;
-                    int nextLine = i + 1;
-                    if (prevLine < 0) prevLine = 0;
-                    if (nextLine > data.Length - 1) nextLine = i;
-                    for (int k = before; k <= after; k++)
+                    if (!string.IsNullOrEmpty(currNum))
                     {
-                        if (data[prevLine][k] != '.' || data[nextLine][k] != '.')
+                        sum += int.Parse(currNum);
+                        currNum = "";
+                    }
+
+                    if (currLine[j] == '.' && !string.IsNullOrEmpty(currNum))
+                    {
+                        int prevLine = i - 1;
+                        int nextLine = i + 1;
+                        if (prevLine < 0) prevLine = 0;
+                        if (nextLine >= data.Length) nextLine = i;
+
+                        for (int k = j; k < currLine.Length && k < data[prevLine].Length && k < data[nextLine].Length; k++)
                         {
-                            sum += int.Parse(currNum);
-                            currNum = "";
-                            break;
+                            if (data[prevLine][k] != '.' || data[nextLine][k] != '.')
+                            {
+                                sum += int.Parse(currNum);
+                                currNum = "";
+                                break;
+                            }
                         }
                     }
                 }
             }
 
-            currNum = "";
+            if (!string.IsNullOrEmpty(currNum))
+            {
+                sum += int.Parse(currNum);
+            }
         }
 
         System.Console.WriteLine(sum);
     }
+
 }
